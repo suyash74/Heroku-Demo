105c6f0c78e727797dfcadcfd83ee9e2aefcb553
suyash74 <45573582+suyash74@users.noreply.github.com>
2020-07-01 07:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
 	output = round(prediction[0], 2)
 
-	return render_template('index.html', prediction_text=f'Salary of Coot should be INR {output}')
+	return render_template('index.html', prediction_text=f'Employee Salary should be INR {output}')
 
 if __name__ == '__main__':
 	app.run(debug=True)
